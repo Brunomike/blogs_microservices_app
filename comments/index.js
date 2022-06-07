5b5e755740a6cbf1cc8e129ccd8d97f0a4f6d3f3
Michael Bruno <brunomike254@gmail.com>
2022-06-07 14:44:27 +0300

    Added an event bus which forwards events to different services once a new resource is created

--- comments/index.js
+++ comments/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 
@@ -6,14 +7,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
-//app.use(cors());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -21,7 +22,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -34,7 +35,21 @@ app.post('/posts/:id/comments', (req, res) => {
 
     commentsByPostId[req.params.id] = comments;
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
+
     res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+    console.log({Received_Event:req.body.type});
+    res.send({});
 });
 
 
--- posts/index.js
+++ posts/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 
@@ -6,14 +7,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');    
-    next();
-});
+// app.use((req,res,next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');    
+//     next();
+// });
 
-//app.use(cors());
+app.use(cors());
 
 const posts = {};
 
@@ -21,7 +22,7 @@ app.get('/posts', (req, res) => {
     res.json(posts);
 });
 
-app.post('/posts', (req, res) => {    
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -29,7 +30,19 @@ app.post('/posts', (req, res) => {
         id, title
     };
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title,
+        }
+    });
+
     res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+    console.log({Received_Event:req.body.type});
+    res.send({});
 });
 
 
