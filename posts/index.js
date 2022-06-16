e183e88116fb0383d8e29dbe85572ff02c9a26a5
Michael Bruno <brunomike254@gmail.com>
2022-06-16 15:33:33 +0300

    Created images of all services present, learnt how to write config files for our objects(deployments,services,e.t.c) and applying them to connect the various pods

--- posts/index.js
+++ posts/index.js
@@ -30,7 +30,8 @@ app.post('/posts', async (req, res) => {
         id, title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    //await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title,
@@ -47,5 +48,6 @@ app.post('/events', (req, res) => {
 
 
 app.listen(4000, () => {
+    console.log('v55');
     console.log('Listening on 4000');
 });
